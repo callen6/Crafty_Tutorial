ad0308a575c039a72a04c65d72f4f453596fb9b3
Cori Allen <coriallen@gmail.com>
2013-12-12 13:25:13 -0500

    Abstract grid, tree, and bush componments into reuseable code in component.js.

---
+++ src/component.js
@@ -0,0 +1,35 @@
+// the grid component allows an element to be located
+// on a grid of tiles
+
+Crafty.c('Grid', {
+	init: function () {
+		this.attr({
+			w: Game.map_grid.tile.width
+			h: Game.map_grid.tile.height
+		})
+	},
+	// locate this entity at the given position on the grid
+	at: function(x,y) {
+		if (x=== undefined && y === undefined) {
+			return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
+		} else {
+			this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height});
+			return this;
+		}
+		}
+	}
+});
+
+Crafty.c('Tree', {
+	init: function () {
+		this.requires('2D, Canvas, Grid, Color');
+		this.color('rgb(20, 125, 40'));
+		},
+});
+
+Crafty.c('Bush', {
+	init: function() {
+		this.requires('2D, Canvas, Grid, Color');
+		this.color('rgb(20, 185, 40');
+	},
+});
