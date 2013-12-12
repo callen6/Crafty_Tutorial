5778f2ad87a0688ed5919ec0334b2c35a0c1d92a
Cori Allen <coriallen@gmail.com>
2013-12-12 12:29:50 -0500

    Create basic file struture, add assets, add crafty.js lib, initialize game in game.js.

---
+++ src/game.js
@@ -0,0 +1,8 @@
+Game = {
+	// initialize and start game
+	start: function() {
+		// start crafty and set a background color so we see its working
+		Crafty.init(480, 320);
+		Crafty.background('green');
+	}
+}
