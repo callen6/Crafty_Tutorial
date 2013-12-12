d6d40b3f8cf8bfbe14541d7c78f1a5b0015f024c
Cori Allen <coriallen@gmail.com>
2013-12-12 13:09:55 -0500

    Add to start function logif to place tree tiles at the edge of the grid and bush tiles randomly within grid.

--- src/game.js
+++ src/game.js
@@ -5,7 +5,7 @@ Game = {
 		height: 16,
 		tile: {
 			width: 16,
-			height: 16,
+			height: 16
 		}
 	},
 	// define the total width of the game screen
@@ -25,5 +25,34 @@ Game = {
 		// start crafty and set a background color so we see its working
 		Crafty.init(Game.width(), Game.height());
 		Crafty.background('rgb(249, 223, 125');
+	// place a tree at every edge square on our grid of 16x16 tiles
+	// use for loop
+	for (var x = 0; x < Game.map_grid.width; x++) {
+		for (var y = 0; y < Game.map_grid.height; y++) {
+			var at_edge = x === 0 || x === Game.map_grid.width -1 || y === 0 || y === Game.map_grid.height -1;
+
+			if (at_edge) {
+				// place a tree entity at the current tile
+				Crafty.e('2D, Canvas, Color')
+				.attr({
+					x: x * Game.map_grid.tile.width,
+					y: y * Game.map_grid.tile.height,
+					w: Game.map_grid.tile.width,
+					h: Game.map_grid.tile.height
+				})
+				.color('rgb(20, 125, 40');
+			} else if (Math.random() < 0.06) {
+				// Place a bush entity at the current tile
+				Crafty.e('2D, Canvas, Color')
+				.attr({
+					x: x * Game.map_grid.tile.width,
+					y: y * Game.map_grid.tile.height,
+					w: Game.map_grid.tile.width,
+					h: Game.map_grid.tile.height
+				})
+				.color('rgb(20, 185, 40)');
+			}
+		}
 	}
+}
 }
