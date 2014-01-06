67261acfa5a2b800bf19c6e9e974c4be373a3824
Cori Allen <coriallen@gmail.com>
2014-01-06 12:19:07 -0500

    Beginning animation on hunter.

--- src/component.js
+++ src/component.js
@@ -44,11 +44,37 @@ Crafty.c('Bush', {
 // this is the player-controlled character
 	Crafty.c('PlayerCharacter', {
 		init: function() {
-			this.requires('Actor, Fourway, Collision, spr_player')
+			this.requires('Actor, Fourway, Collision, spr_player, SpriteAnimation')
 				.fourway(4)
 				.stopOnSolids()
 				 // Whenever the PC touches a village, respond to the event
       	.onHit('Village', this.visitVillage);
+      	// on the next four lines define our four animations
+      	// each call to .animate specifies:
+      	// the name of the animation
+      	// the x and y coordinates within the sprite map at which the animation set begins
+      	// the number of animation frames in addition to  the first one
+      	.animate('PlayerMovingUp', 0,0,2)
+      	.animate('PlayerMovingRight', 0,1,2)
+      	.animate('PlayerMovingDown', 0,2,2)
+      	.animate('PlayerMovingLeft', 0,3,2);
+
+				// watch for a change of direction and switch animations accordingly
+				var animation_speed = 8;
+				this.bind('NewDirection', function(data){
+					if (data.x > 0) {
+						this.animate('PlayerMovingRight', animation_speed, -1);
+					} else if (data.x < 0 {
+						this.animate('PlayerMovingLeft', animation_speed, -1);
+					} else if (data.y > 0){
+						this.animate('PlayerMovingDown', animation_speed, -1);
+					} else if (data.y < 0) {
+						this.animate('PlayerMovingUp', animation_speed, -1);
+					} else {
+						this.stop();
+					}
+				});
+
 		},
 // this registers a stop-movement function to be called
 //  when this entity hits an entity with the "solid" component
